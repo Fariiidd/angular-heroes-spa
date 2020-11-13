be1ee9a6bbfbdd12b2a748bc94c76c9f2d829eb1
@farid <desarrollo@sabiogo.com>
2020-11-13 17:06:42 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+//Routes
+import { APP_ROUTES } from './app.routes';
 
+//Service
+
+//Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTES,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+export const APP_ROUTES: Routes = [  
+    { path: 'home', component: HomeComponent},
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
+
+]
